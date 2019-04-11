93f9b7e68af790eeca7befecbf8a73a99209ff25
Yeshi <tyeshi777@yahoo.com>
2019-04-11 12:36:35 -0700

    first commit

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>hello there</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
